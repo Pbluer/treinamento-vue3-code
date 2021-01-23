c8fdd02fecdf9fa297c4d4ec369fb47fe9a26f7e
RamonL <46079563+Pbluer@users.noreply.github.com>
2021-01-22 23:29:23 -0300

    problema no css

---
+++ dashboard/src/views/Home/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 class="font-black">Aqui é a home</h1>
+  <h1 class="font-black text-brand-main">Aqui é a home</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
